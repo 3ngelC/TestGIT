5f0c307960b8a9b2b339485d26e954125936f473
Angel Cachi <Angel.Cachi@jalasoft.com>
2022-07-30 18:08:16 -0400

    First Changes

---
+++ src/main/java/com/mycompany/testgit/Colors.java
@@ -0,0 +1,36 @@
+package com.mycompany.testgit;
+
+import java.util.Scanner;
+
+
+//test colors//
+
+//just testing//
+
+    public class Colors {
+        public static void main(String[]args){
+            Scanner entrada = new Scanner(System.in);
+            String Color;
+
+            System.out.println("Insert a color: ");
+            Color = entrada.next();
+
+            System.out.println("el color es: "+Color);
+        }
+    }
+/*
+
+function Color(){
+    console.log ("Color")
+}
+
+ */
+
+
+//test 1 update//
+
+
+/*
+second change update
+ */
+
